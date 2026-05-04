1600e38e5e5524da5fe542c76ba365d9b3a63322
Takoua Naceur <outaytakwa1@gmail.com>
2026-05-04 22:53:30 +0100

    fix: update apiUrl to new backend URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://esprit-pi-fullstack-js-4-twin-8-202.vercel.app/_/backend'
+  apiUrl: 'https://esprit-pi-fullstack-js-4-twin-8-202-ten.vercel.app'
 };
